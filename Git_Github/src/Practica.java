952b29030895975cc3035faefdfd189b5328b0ff
Laura Marin <lalamarin0927@gmail.com>
2023-03-23 18:23:55 +0100

    Integrantes del grupo

--- Git_Github/src/Practica.java
+++ Git_Github/src/Practica.java
@@ -4,7 +4,15 @@ public class Practica {
 	public static void main(String[] args) {
 		System.out.println("Hola , estamos aprendiendo a usar Git y GitHub en Eclipse");
 		System.out.println("Clase de Entornos de Desarrollo");
+		String[] nombres = {"Javier", "Miriam", "Ryan", "Laura"};
+        
+        System.out.println("Los integrantes del grupo son:");
+        
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
 
 	}
 
-}
+
